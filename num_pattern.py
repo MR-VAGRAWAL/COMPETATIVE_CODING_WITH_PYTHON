fa0258e776e90f1a0fc8ec64118ff568de14bf50
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-11 22:09:02 +0530

    First Number Pattern

---
+++ num_pattern.py
@@ -0,0 +1,8 @@
+for i in range(4,1,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+for k in range(1,5):
+    for l  in range(1,k+1):
+        print(l,end="")
+    print()    
